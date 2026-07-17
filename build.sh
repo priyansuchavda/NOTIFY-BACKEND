869cdc457c1a5c39ef31ca2740bc3d6fd6c31ade
Priyanshu Chavda <priyanshu@16arena.com>
2026-07-17 13:12:20 +0530

    Enforce Python 3.11 in build and add .python-version

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "==> Python version"
+python --version
+python -c "import sys; assert sys.version_info[:2] == (3, 11), f'Need Python 3.11, got {sys.version}'; print('Python 3.11 OK')"
+
 echo "==> Installing dependencies"
 pip install -r requirements.txt
 
